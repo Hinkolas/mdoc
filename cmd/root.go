a14cbcffb9abe2a305596aece052351f0ee5c567
Nicholas Hinke <hinke.nicholas@icloud.com>
2026-01-09 19:22:21 +0100

    remove root command logic

--- cmd/root.go
+++ cmd/root.go
@@ -2,11 +2,9 @@ package cmd
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"runtime"
 
-	"github.com/hinkolas/mdoc/src"
 	"github.com/spf13/cobra"
 )
 
@@ -21,33 +19,6 @@ var rootCmd = &cobra.Command{
 	Version: fmt.Sprintf("%s, %s/%s", "0.0.1", runtime.GOOS, runtime.GOARCH),
 	Use:     "mdoc",
 	Short:   "An easy to use cli tool for turning your markdown files into good looking PDFs with customizable templates and more. ",
-	Run: func(cmd *cobra.Command, args []string) {
-
-		// Return error if no file was provided
-		if len(args) != 1 {
-			fmt.Println("Please provide exactly 1 markdown file to convert.")
-			os.Exit(1)
-		}
-
-		inputPath := args[0]
-		file, err := os.Open(inputPath)
-		if err != nil {
-			fmt.Printf("Error opening file: %v\n", err)
-			os.Exit(1)
-		}
-		defer file.Close()
-
-		input, err := io.ReadAll(file)
-		if err != nil {
-			fmt.Printf("Error reading file: %v\n", err)
-			os.Exit(1)
-		}
-
-		src.Create(string(input))
-
-		os.Exit(0)
-
-	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
